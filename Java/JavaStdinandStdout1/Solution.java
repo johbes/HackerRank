503b97f4359d9476484dd2aa15aab55304ade937
Johan <johan_besseling@hotmail.com>
2020-12-10 13:13:18 +0100

    refactored files

--- Java/JavaStdinandStdout1/Solution.java
+++ Java/JavaStdinandStdout1/Solution.java
@@ -4,12 +4,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
-        
-        System.out.println(a);
-        // Complete this line
-        // Complete this line
+        for(int i = 0;i < 3;i++){
+            System.out.println(scan.nextInt());
+        }
     }
 }
